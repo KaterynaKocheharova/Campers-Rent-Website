c671e0c7d65a539c35b1ffa0f4ec1bd9bcbb3bdf
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-09-25 23:33:25 +0300

    finish her

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -8,3 +8,17 @@ const CatalogPage = () => {
 }
 
 export default CatalogPage;
+
+// Section components
+
+// do markup of the left side
+// set up filters slice
+// finish and test the left part
+
+// set up async redux for all vehicles
+// test
+// do markup
+
+// set up favorite slice
+// finish page markup
+
