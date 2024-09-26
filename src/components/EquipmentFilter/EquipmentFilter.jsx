32de6695ca87f41f02c46ae34a306390dc0cb53e
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-09-26 17:34:36 +0300

    implement equipment filter feature

--- src/components/EquipmentFilter/EquipmentFilter.jsx
+++ src/components/EquipmentFilter/EquipmentFilter.jsx
@@ -1,3 +1,6 @@
+import { useSelector, useDispatch } from "react-redux";
+import { selectVehicleEquipmentFilter } from "../../redux/filters/selectors";
+import { changeVehicleEquipmentFilter } from "../../redux/filters/slice";
 import FilterButtonsGrid from "../FilterButtonsGrid/FilterButtonsGrid";
 import FilterButton from "../FilterButton/FilterButton";
 import FilterTitle from "../FilterTitle/FilterTitle";
@@ -26,12 +29,22 @@ const equipmentFilters = [
 ];
 
 const EquipmentFilter = () => {
+  const equipment = useSelector(selectVehicleEquipmentFilter);
+  const dispatch = useDispatch();
+
+  const handleChangeVehicleEquipmentFilter = (clickedEquipment) => {
+    dispatch(changeVehicleEquipmentFilter(clickedEquipment));
+  };
   return (
     <>
       <FilterTitle>Vehicle equipment</FilterTitle>
       <FilterButtonsGrid>
         {equipmentFilters.map((equipmentFilter, index) => (
           <FilterButton
+            isSelected={equipment.includes(equipmentFilter.name)}
+            changeFilterHandler={() =>
+              handleChangeVehicleEquipmentFilter(equipmentFilter.name)
+            }
             key={index}
             filterData={equipmentFilter}
             variant="equipment"
