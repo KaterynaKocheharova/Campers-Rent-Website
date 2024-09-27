9bfb849eb78f5f33fb5e2a37b0cc9045de3e956d
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-09-28 00:30:29 +0300

    fix filter button styles - the text now wraps

--- src/components/EquipmentFilter/EquipmentFilter.jsx
+++ src/components/EquipmentFilter/EquipmentFilter.jsx
@@ -1,3 +1,87 @@
+// import { useSelector, useDispatch } from "react-redux";
+// import {
+//   selectVehicleEquipmentFilter,
+//   selectVehicleTransmissionFilter,
+// } from "../../redux/filters/selectors";
+// import {
+//   changeVehicleEquipmentFilter,
+//   changeVehicleTransmissionFilter,
+// } from "../../redux/filters/slice";
+// import FilterButtonsGrid from "../FilterButtonsGrid/FilterButtonsGrid";
+// import FilterButton from "../FilterButton/FilterButton";
+// import FilterTitle from "../FilterTitle/FilterTitle";
+
+// export const equipmentFilters = [
+//   {
+//     name: "AC",
+//     icon: "icon-wind",
+//   },
+//   { name: "transmission", icons: "icon-diagram" },
+//   {
+//     name: "kitchen",
+//     icon: "icon-coffee",
+//   },
+//   {
+//     name: "TV",
+//     icon: "icon-tv",
+//   },
+//   {
+//     name: "bathroom",
+//     icon: "icon-droplet",
+//   },
+// ];
+
+// const EquipmentFilter = () => {
+//   const equipment = useSelector(selectVehicleEquipmentFilter);
+//   const vehicleTransmissionFilter = useSelector(
+//     selectVehicleTransmissionFilter
+//   );
+//   const dispatch = useDispatch();
+
+//   const handleChangeVehicleEquipmentFilter = (clickedEquipment) => {
+//     dispatch(changeVehicleEquipmentFilter(clickedEquipment));
+//   };
+//   return (
+//     <>
+//       <FilterTitle>Vehicle equipment</FilterTitle>
+//       <FilterButtonsGrid>
+//         {equipmentFilters.map((equipmentFilter, index) => {
+//           if(equipmentFilter.name === "transmission") {
+// return (
+//   <FilterButton
+//           isSelected={vehicleTransmissionFilter === "Automatic"}
+//           variant="equipment"
+//           changeFilterHandler={() =>
+//             dispatch(changeVehicleTransmissionFilter())
+//           }
+//           filterData={{ icon: "icon-diagram" }}
+//         />
+
+// )
+//           } else {
+// return (
+// <FilterButton
+//             isSelected={equipment.includes(equipmentFilter.name)}
+//             changeFilterHandler={() =>
+//               handleChangeVehicleEquipmentFilter(equipmentFilter.name)
+//             }
+//             key={index}
+//             filterData={equipmentFilter}
+//             variant="equipment"
+//           />
+
+// )
+
+//           }
+
+//         }
+//       </FilterButtonsGrid>
+//     </>
+//   );
+// };
+
+// export default EquipmentFilter;
+
 import { useSelector, useDispatch } from "react-redux";
 import {
   selectVehicleEquipmentFilter,
@@ -16,6 +100,7 @@ export const equipmentFilters = [
     name: "AC",
     icon: "icon-wind",
   },
+  { name: "transmission", icon: "icon-diagram" },
   {
     name: "kitchen",
     icon: "icon-coffee",
@@ -40,29 +125,38 @@ const EquipmentFilter = () => {
   const handleChangeVehicleEquipmentFilter = (clickedEquipment) => {
     dispatch(changeVehicleEquipmentFilter(clickedEquipment));
   };
+
   return (
     <>
       <FilterTitle>Vehicle equipment</FilterTitle>
       <FilterButtonsGrid>
-        {equipmentFilters.map((equipmentFilter, index) => (
-          <FilterButton
-            isSelected={equipment.includes(equipmentFilter.name)}
-            changeFilterHandler={() =>
-              handleChangeVehicleEquipmentFilter(equipmentFilter.name)
-            }
-            key={index}
-            filterData={equipmentFilter}
-            variant="equipment"
-          />
-        ))}
-        <FilterButton
-          isSelected={vehicleTransmissionFilter === "Automatic"}
-          variant="equipment"
-          changeFilterHandler={() =>
-            dispatch(changeVehicleTransmissionFilter())
+        {equipmentFilters.map((equipmentFilter, index) => {
+          if (equipmentFilter.name === "transmission") {
+            return (
+              <FilterButton
+                key={index}
+                isSelected={vehicleTransmissionFilter === "Automatic"}
+                variant="equipment"
+                changeFilterHandler={() =>
+                  dispatch(changeVehicleTransmissionFilter())
+                }
+                filterData={equipmentFilter}
+              />
+            );
+          } else {
+            return (
+              <FilterButton
+                key={index}
+                isSelected={equipment.includes(equipmentFilter.name)}
+                changeFilterHandler={() =>
+                  handleChangeVehicleEquipmentFilter(equipmentFilter.name)
+                }
+                filterData={equipmentFilter}
+                variant="equipment"
+              />
+            );
           }
-          filterData={{ icon: "icon-diagram" }}
-        />
+        })}
       </FilterButtonsGrid>
     </>
   );
--- src/components/FilterButton/FilterButton.jsx
+++ src/components/FilterButton/FilterButton.jsx
@@ -4,19 +4,17 @@ import css from "./FilterButton.module.css";
 const FilterButton = ({
   isSelected,
   changeFilterHandler,
-  filterData: { icon },
+  filterData: { icon, name },
 }) => {
   return (
     <button
       className={clsx(css.button, isSelected && css["selected-button"])}
       onClick={changeFilterHandler}
     >
-      <div className={css.flex}>
         <svg className={css.icon}>
           <use href={`/sprite.svg#${icon}`}></use>
         </svg>
         <p className={css.text}>{name}</p>
-      </div>
     </button>
   );
 };
