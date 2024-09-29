b85c323e2754a683f17625cf45f1739476215433
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-09-29 15:39:38 +0300

    format code in vehicle details tabs

--- src/components/VehicleDetailsTabs/buildLinkClass.js
+++ src/components/VehicleDetailsTabs/buildLinkClass.js
@@ -2,5 +2,5 @@ import css from "./VehicleDetailsTabs.module.css";
 import clsx from "clsx";
 
 export const buildLinkClass = ({ isActive }) => {
-    return clsx(css["link"], isActive && css["active-link"]);
-  };
+  return clsx(css["link"], isActive && css["active-link"]);
+};
