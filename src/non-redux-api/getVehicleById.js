615a8e4f83d39258711b258ae0c5dabf114bd896
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-09-29 15:57:05 +0300

    add comments to getVehicleById

--- src/non-redux-api/getVehicleById.js
+++ src/non-redux-api/getVehicleById.js
@@ -2,4 +2,6 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io";
 
+// I don't store current vehicle details in redux, so here is the function
+
 export const getVehicleById = async (id) => axios.get(`/campers/${id}`);
