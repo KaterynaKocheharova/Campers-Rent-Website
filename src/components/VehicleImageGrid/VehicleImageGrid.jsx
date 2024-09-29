4c38418b571fe1da8b6ccac3997615f4665a7c43
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-09-29 15:48:17 +0300

    add comment to the vehicle image grid

--- src/components/VehicleImageGrid/VehicleImageGrid.jsx
+++ src/components/VehicleImageGrid/VehicleImageGrid.jsx
@@ -4,6 +4,9 @@ import css from "./VehicleImageGrid.module.css";
 const VehicleImageGrid = ({ images }) => {
   return (
     <ul className={css.grid}>
+      {/* The data returned doesn't provide 4 pcitures as the mockup provides - 
+      instead it has three items in the gallery array each of them
+      having two identical pics - so here I'm repeating the first pic in order to match the mockup */}
       {images.map((image, index) => (
         <VehicleImage key={index} url={image} />
       ))}
