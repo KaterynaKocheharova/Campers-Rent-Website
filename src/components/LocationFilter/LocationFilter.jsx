82caf467876828a4cbe8e07aff509a6e9dcdfde7
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-01 12:05:53 +0300

    make a fetch req on enter

--- src/components/LocationFilter/LocationFilter.jsx
+++ src/components/LocationFilter/LocationFilter.jsx
@@ -1,6 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
-import { selectLocationFilter } from "../../redux/filters/selectors";
+import {
+  selectLocationFilter,
+  selectVehicleEquipmentFilter,
+  selectVehicleTypeFilter,
+  selectVehicleTransmissionFilter,
+} from "../../redux/filters/selectors";
 import { changeLocationFilter } from "../../redux/filters/slice";
+import { fetchVehicles } from "../../redux/vehicles/operations";
 import { useId } from "react";
 import css from "./LocationFilter.module.css";
 
@@ -8,10 +14,31 @@ const LocationFilter = () => {
   const filterInputId = useId();
 
   const location = useSelector(selectLocationFilter);
+  const vehicleEquipment = useSelector(selectVehicleEquipmentFilter);
+  const vehicleType = useSelector(selectVehicleTypeFilter);
+  const transmission = useSelector(selectVehicleTransmissionFilter);
+
   const dispatch = useDispatch();
 
   const handleLocationFilterChange = (value) => {
     dispatch(changeLocationFilter(value));
+  };
+
+  const handleEnterClick = (e) => {
+    if (e.key === "Enter") {
+      dispatch(
+        fetchVehicles({
+          page: 1,
+          limit: 4,
+          filters: {
+            location,
+            vehicleEquipment,
+            vehicleType,
+            transmission,
+          },
+        })
+      );
+    }
   };
 
   return (
@@ -29,6 +56,7 @@ const LocationFilter = () => {
           onChange={(e) => {
             handleLocationFilterChange(e.target.value);
           }}
+          onKeyDown={handleEnterClick}
         />
         <svg className={css["map-icon"]}>
           <use href="/sprite.svg#icon-map"></use>
