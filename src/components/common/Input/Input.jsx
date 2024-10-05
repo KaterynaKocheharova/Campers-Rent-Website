b7cd0e07fdcafb5bb3fafe1e20fca27dc638a455
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-05 20:08:46 +0300

    simplify text reusable component

--- src/components/common/Input/Input.jsx
+++
@@ -1,8 +0,0 @@
-import { Field } from "formik";
-import css from "./Input.module.css";
-
-const Input = ({ name, type, id }) => {
-  return <Field name={name} type={type} id={id} className={css.input} />;
-};
-
-export default Input;
--- src/components/common/Text/Text.jsx
+++ src/components/common/Text/Text.jsx
@@ -1,22 +1,9 @@
 import clsx from "clsx";
 import css from "./Text.module.css";
 
-const Text = ({
-  children,
-  variant,
-  extraClass,
-}) => {
+const Text = ({ children, variant, extraClass }) => {
   return (
-    <p
-      className={clsx(
-        css.text,
-        variant === "dark" && css.dark,
-        variant === "light" && css.light,
-        extraClass && css[extraClass]
-      )}
-    >
-      {children}
-    </p>
+    <p className={clsx(css.text, css[variant], css[extraClass])}>{children}</p>
   );
 };
 
