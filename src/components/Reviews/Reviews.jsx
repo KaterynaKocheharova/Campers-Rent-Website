7995344c336a04b0a0b736e4d70ba38b71f03aa9
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-06 17:18:02 +0300

    add scroll to reviews page

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,14 +1,18 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { useFetchVehicleDetails } from "../../hooks/useFetchVehicleDetails";
 import ReviewItem from "../ReviewItem/ReviewItem";
+import { scrollToSubpage } from "../../utils/scrollToSubpage";
 import css from "./Reviews.module.css";
 
 const Reviews = () => {
   const { vehicleData } = useFetchVehicleDetails();
-
   const { reviews } = vehicleData || {};
 
   const componentRef = useRef();
+
+  useEffect(() => {
+    scrollToSubpage(componentRef.current);
+  }, []);
 
   return (
     <ul ref={componentRef} className={css.list}>
