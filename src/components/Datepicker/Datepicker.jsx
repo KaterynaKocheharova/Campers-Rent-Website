efcf648aa52daa9b29f28828e6b7eaa62afc2679
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-06 13:34:30 +0300

    adjust list of days in datepicker - now each day short version is created with 3 letters and days start with Monday

--- src/components/Datepicker/Datepicker.jsx
+++ src/components/Datepicker/Datepicker.jsx
@@ -2,10 +2,28 @@ import DatePicker from "react-datepicker";
 import { CiCalendar } from "react-icons/ci";
 import "react-datepicker/dist/react-datepicker.css";
 import "./Dtepicker.css";
+import { registerLocale } from "react-datepicker";
+import { enUS } from "date-fns/locale";
+import { format } from "date-fns";
 
 const Datepicker = ({ selected, onChange, isError }) => {
+  const customLocale = {
+    ...enUS,
+    options: {
+      ...enUS.options,
+      weekStartsOn: 1,
+    },
+    localize: {
+      ...enUS.localize,
+      day: (n) => format(new Date(2023, 0, n + 1), "EEE"),
+    },
+  };
+
+  registerLocale("custom-en", customLocale);
+
   return (
     <DatePicker
+      locale="custom-en"
       className={isError ? "error-input" : ""}
       showIcon
       icon={<CiCalendar />}
